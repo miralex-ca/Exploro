7acbd3a36f9a46c7401767cf85ee43a772daf3bf
Aleksandr Mironov <aleksandr.mironov@ebox.ca>
2026-05-19 21:07:10 -0400

    Update the default value for `savedLevel1URI` in `MySettings` to "home:nul".

--- data/repository/src/commonMain/kotlin/com/muralex/data/repository/sources/localsettings/MySettings.kt
+++ data/repository/src/commonMain/kotlin/com/muralex/data/repository/sources/localsettings/MySettings.kt
@@ -8,7 +8,7 @@ import com.russhwolf.settings.string
 
 class MySettings (s : Settings) {
     var listCacheTimestamp by s.long(defaultValue = 0)
-    var savedLevel1URI by s.string(defaultValue = "Level1Navigation.AllCountries.screenIdentifier.URI")
+    var savedLevel1URI by s.string(defaultValue = "home:nul")
     var dbVersion by s.long(defaultValue = 1)
     var shouldForceDbUpdate by s.boolean(defaultValue = false)
     var themeModeId by s.int(defaultValue = 0)
