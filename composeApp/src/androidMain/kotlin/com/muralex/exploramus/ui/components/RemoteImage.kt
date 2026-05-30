ad0927b8ff9dbb26c695d425b86331b2b5f008a0
Aleksandr Mironov <aleksandr.mironov@ebox.ca>
2026-05-30 01:00:44 -0400

    Implement conditional error placeholders in the `RemoteImage` component

--- composeApp/src/androidMain/kotlin/com/muralex/exploramus/ui/components/RemoteImage.kt
+++ composeApp/src/androidMain/kotlin/com/muralex/exploramus/ui/components/RemoteImage.kt
@@ -1,17 +1,19 @@
 package com.muralex.exploramus.ui.components
 
 
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
-
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import coil.compose.AsyncImage
 import coil.request.CachePolicy
 import coil.request.ImageRequest
+import com.muralex.exploramus.R
 
 
 @Composable
@@ -23,6 +25,11 @@ fun RemoteImage(
     alpha: Float = 1f,
     shape: Shape = RectangleShape,
 ) {
+    val placeholderRes = when {
+        imageUrl?.lowercase()?.contains("taliban") == true  -> R.drawable.taliban_flag
+        else -> R.drawable.flag_placeholder
+    }
+
     AsyncImage(
         model = ImageRequest.Builder(LocalContext.current)
             .data(imageUrl)
@@ -31,6 +38,7 @@ fun RemoteImage(
             .diskCachePolicy(CachePolicy.ENABLED)
             .networkCachePolicy(CachePolicy.ENABLED)
             .build(),
+        error = painterResource(placeholderRes),
         contentDescription = contentDescription,
         modifier = modifier
             .clip(shape),
