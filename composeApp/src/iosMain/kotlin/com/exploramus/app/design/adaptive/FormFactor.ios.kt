d1aa3ec9b9faf927ab91b93dba7b17e0baead482
Aleksandr Mironov <aleksandr.mironov@ebox.ca>
2026-08-15 22:38:59 -0400

    Implement iOS `flagAssetUri` to resolve flag image paths from the main bundle.

--- composeApp/src/iosMain/kotlin/com/exploramus/app/design/adaptive/FormFactor.ios.kt
+++ composeApp/src/iosMain/kotlin/com/exploramus/app/design/adaptive/FormFactor.ios.kt
@@ -3,7 +3,9 @@ package com.exploramus.app.design.adaptive
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalWindowInfo
+import com.exploramus.core.common.logging.Log
 import platform.UIKit.UIDevice
 import platform.UIKit.UIUserInterfaceIdiomPad
 
@@ -12,12 +14,13 @@ import platform.UIKit.UIUserInterfaceIdiomPad
 actual fun rememberFormFactor(): FormFactor {
     val windowInfo = LocalWindowInfo.current
     val containerSize = windowInfo.containerSize
+    val density = LocalDensity.current
     
-    return remember(containerSize) {
+    return remember(containerSize, density) {
         val isPad = UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad
         
-        val w = containerSize.width
-        val h = containerSize.height
+        val w = with(density) { containerSize.width.toDp().value }
+        val h = with(density) { containerSize.height.toDp().value }
         
         val widthType = when {
             w < 600 -> WidthType.COMPACT
@@ -38,6 +41,8 @@ actual fun rememberFormFactor(): FormFactor {
             heightType = heightType,
             orientation = orientation,
             isLargeTablet = isPad && (w > 1000 || h > 1000)
-        )
+        ).also {
+            Log.d("FormFactor: $it, width: $w, height: $h")
+        }
     }
 }
