08af1344b9f413698b5c1853be4184e026f7e11c
Aleksandr Mironov <aleksandr.mironov@ebox.ca>
2026-08-15 22:21:27 -0400

    Implement iOS `flagAssetUri` to resolve flag image paths from the main bundle.

--- composeApp/src/iosMain/kotlin/com/exploramus/app/composables/components/RemoteImage.ios.kt
+++ composeApp/src/iosMain/kotlin/com/exploramus/app/composables/components/RemoteImage.ios.kt
@@ -1,6 +1,20 @@
 package com.exploramus.app.composables.components
 
 import androidx.compose.runtime.Composable
+import platform.Foundation.NSBundle
+import platform.Foundation.NSLog
 
 @Composable
-actual fun flagAssetUri(countryCode: String): String = ""
+actual fun flagAssetUri(countryCode: String): String {
+    val name = countryCode.lowercase()
+    // Based on the project structure and PBXFileSystemSynchronizedRootGroup in Xcode 16,
+    // assets in AppRawData/flags/w640 are likely flattened into the bundle root.
+    val path = NSBundle.mainBundle.pathForResource(name, "png")
+    
+    return if (path != null) {
+        "file://$path"
+    } else {
+        NSLog("WARNING: Flag asset not found for country code: $countryCode")
+        ""
+    }
+}
