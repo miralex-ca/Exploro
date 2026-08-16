9e713dc8da31a7064f5e229a0175dc3f3e408016
Aleksandr Mironov <aleksandr.mironov@ebox.ca>
2026-08-16 17:43:42 -0400

    Cleanup in ios app

--- data/localdb/src/commonMain/kotlin/com/exploramus/data/localdb/SQLDelightLocalDataSource.kt
+++ data/localdb/src/commonMain/kotlin/com/exploramus/data/localdb/SQLDelightLocalDataSource.kt
@@ -7,7 +7,7 @@ import com.exploramus.data.common.LocalDataSource
 
 internal object DatabaseConfig {
     const val NAME = "applocal.db"
-    const val VERSION = 12L
+    const val VERSION = 14L
 }
 
 internal fun createLocalDataSource(sqlDriver: SqlDriver): LocalDataSource {
